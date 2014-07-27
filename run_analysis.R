29d1d598271f00c731bfe2ec3beca76f5da64594
codeforcalifornia <codeforcalifornia@users.noreply.github.com>
2014-07-27 15:28:24 -0700

    Properly compute mean values.

--- run_analysis.R
+++ run_analysis.R
@@ -16,9 +16,11 @@ y_data <- c(y_train_raw[,1], y_test_raw[,1])
 s_data <- c(s_train_raw[,1], s_test_raw[,1])
 
 # Replace numbers in the activity data set with informative labels
+# This will give descriptive activity names to name the activities in the data set.
 activity_raw <- read.table("UCI HAR Dataset/activity_labels.txt")
+y_data_labeled <- character(nrow(activity_raw))
 for (activity in 1:nrow(activity_raw)) {
-    y_data[y_data == activity] <- as.character(activity_raw[activity,2])
+    y_data_labeled[y_data == activity] <- as.character(activity_raw[activity,2])
 }
 
 # Merge X test and train data sets
@@ -32,47 +34,62 @@ for (xcol in 1:ncol(x_train_raw)) {
 # We will now create and populate the tidy data set.
 tidy_data <- data.frame(x_data)
 
+# Label the features in the data set with descriptive variable names
+features_raw <- read.table("UCI HAR Dataset/features.txt")
+features_dat <- features_raw[,2]
+for (xcol in seq(1:ncol(x_data))) {
+    names(tidy_data)[xcol] <- as.character(features_dat[xcol])
+}
+
 # Extract the mean and standard deviation for each measurement
 # Instead of using %in%, just hard-code the columns that are known to have
 # standard deviation and mean values
 ms_cols <- c(1, 2, 3, 41, 42, 43, 81, 82, 83, 121, 122, 123, 161, 162, 163, 201, 214, 227, 240, 253, 266, 267, 268, 345, 346, 347, 424, 425, 426, 503, 516, 529, 542, 4, 5, 6, 44, 45, 46, 84, 85, 86, 124, 125, 126, 164, 165, 166, 202, 215, 228, 241, 254, 269, 270, 271, 348, 349, 350, 427, 428, 429, 504, 517, 530, 543)
 tidy_data <- tidy_data[,ms_cols]
 
-# Label the features in the data set
-features_raw <- read.table("UCI HAR Dataset/features.txt")
-features_dat <- features_raw[,2]
-for (xcol in seq(1:length(ms_cols))) {
-    names(tidy_data)[xcol] <- as.character(features_dat[ms_cols[xcol]])
-}
-
 # Append activity and subject data to the data set.
-tidy_data[,67] <- y_data
-tidy_data[,68] <- s_data
-names(tidy_data)[67] <- "activity"
-names(tidy_data)[68] <- "subject"
+tidy_data[,ncol(ms_cols)+1] <- y_data_labeled
+tidy_data[,ncol(ms_cols)+2] <- s_data
+names(tidy_data)[ncol(ms_cols)+1] <- "activity"
+names(tidy_data)[ncol(ms_cols)+2] <- "subject"
 
 # Write out the tidy data set
+# Since we will only be submitting the 2nd data set, do not print this one out.
 write.table(tidy_data, file="tidy_data.txt")
 
 subjects <- c(1, 11, 14, 15, 16, 17, 19, 21, 22, 23, 25, 26, 27, 28, 29, 3, 30, 5, 6, 7, 8)
 
 activities <- c(1, 2, 3, 4, 5, 6)
 
-ds2 <- matrix(nrow=(length(subjects) * length(activities)), ncol=(length(ms_cols) + 2))
+ds2 <- matrix(nrow=(length(subjects) * length(activities)), ncol=(ncol(x_data) + 2))
 
+# For each subject ...
 for (subject_idx in 1:length(subjects)) {
+    # For each activity ...
     for (activity in activities) {
-        for (variable in 1:length(ms_cols)) {
-            subscript <- ((subject_idx - 1) * length(activities)) + activity
-            data_mean <- mean(tidy_data[(tidy_data[,67] == activity) & (tidy_data[,68] == subjects[subject_idx]),])
-            #message(paste(c(subscript, ' ', subject_idx, ' ', activity, ' ', variable)))
-            ds2[subscript, variable] <- data_mean
-            ds2[subscript,67] <- activity
-            ds2[subscript,68] <- subjects[subject_idx]
+        # Determine the subscript where we will store our results
+        subscript <- ((subject_idx - 1) * length(activities)) + activity
+        x_subset <- x_data[(y_data == activity) & (s_data == subjects[subject_idx]),]
+        for (xcol in 1:ncol(x_subset)) {
+            ds2[subscript,xcol] <- mean(x_subset[,xcol])
         }
+        ds2[subscript,ncol(x_data) + 1] <- activity
+        ds2[subscript,ncol(x_data) + 2] <- subjects[subject_idx]
+        print('Incoming data mean')
+        print(ds2[subscript,])
+        print('End data mean')
     }
 }
 
 ds2 <- data.frame(ds2)
-names(ds2) <- names(tidy_data)
+for (xcol in seq(1:ncol(x_data))) {
+    names(ds2)[xcol] <- as.character(features_dat[xcol])
+}
+
+for (activity in 1:nrow(activity_raw)) {
+    ds2[ds2[,ncol(x_data) + 1] == activity, ncol(x_data) + 1] <- as.character(activity_raw[activity,2])
+}
+
+names(ds2)[ncol(x_data) + 1] <- 'activity'
+names(ds2)[ncol(x_data) + 2] <- 'subject.id'
 write.table(ds2, file="tidy_derived_data.txt")
