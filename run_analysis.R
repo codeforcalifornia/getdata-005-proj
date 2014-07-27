10dd0bf72c6724ab977ca45fe474a2e675876af2
codeforcalifornia <codeforcalifornia@users.noreply.github.com>
2014-07-27 12:23:52 -0700

    Add run_analysis.sh, add comments to run_analysis.R.

--- run_analysis.R
+++ run_analysis.R
@@ -10,40 +10,48 @@ x_train_raw <- read.table("UCI HAR Dataset/train/X_train.txt")
 y_train_raw <- read.table("UCI HAR Dataset/train/y_train.txt")
 s_train_raw <- read.table("UCI HAR Dataset/train/subject_train.txt")
 
+# Merge activity test and train data sets
 y_data <- c(y_train_raw[,1], y_test_raw[,1])
+# Merge subject test and train data sets
 s_data <- c(s_train_raw[,1], s_test_raw[,1])
 
+# Replace numbers in the activity data set with informative labels
 activity_raw <- read.table("UCI HAR Dataset/activity_labels.txt")
-
 for (activity in 1:nrow(activity_raw)) {
     y_data[y_data == activity] <- as.character(activity_raw[activity,2])
 }
 
+# Merge X test and train data sets
 x_data <- matrix(nrow=(nrow(x_test_raw) + nrow(x_train_raw)), ncol=ncol(x_train_raw))
-
 for (xcol in 1:ncol(x_train_raw)) {
     x_data[,xcol] <- c(x_train_raw[,xcol], x_test_raw[,xcol])
 }
 
-# Instead of using %in%, just hard-code the columns that are known to have std
-# and mean values
-ms_cols <- c(1, 2, 3, 41, 42, 43, 81, 82, 83, 121, 122, 123, 161, 162, 163, 201, 214, 227, 240, 253, 266, 267, 268, 345, 346, 347, 424, 425, 426, 503, 516, 529, 542, 4, 5, 6, 44, 45, 46, 84, 85, 86, 124, 125, 126, 164, 165, 166, 202, 215, 228, 241, 254, 269, 270, 271, 348, 349, 350, 427, 428, 429, 504, 517, 530, 543)
+# At this point, we have merged the test and train data sets.
 
+# We will now create and populate the tidy data set.
 tidy_data <- data.frame(x_data)
+
+# Extract the mean and standard deviation for each measurement
+# Instead of using %in%, just hard-code the columns that are known to have
+# standard deviation and mean values
+ms_cols <- c(1, 2, 3, 41, 42, 43, 81, 82, 83, 121, 122, 123, 161, 162, 163, 201, 214, 227, 240, 253, 266, 267, 268, 345, 346, 347, 424, 425, 426, 503, 516, 529, 542, 4, 5, 6, 44, 45, 46, 84, 85, 86, 124, 125, 126, 164, 165, 166, 202, 215, 228, 241, 254, 269, 270, 271, 348, 349, 350, 427, 428, 429, 504, 517, 530, 543)
 tidy_data <- tidy_data[,ms_cols]
 
+# Label the features in the data set
 features_raw <- read.table("UCI HAR Dataset/features.txt")
 features_dat <- features_raw[,2]
-
 for (xcol in seq(1:length(ms_cols))) {
     names(tidy_data)[xcol] <- as.character(features_dat[ms_cols[xcol]])
 }
 
+# Append activity and subject data to the data set.
 tidy_data[,67] <- y_data
 tidy_data[,68] <- s_data
 names(tidy_data)[67] <- "activity"
 names(tidy_data)[68] <- "subject"
 
+# Write out the tidy data set
 write.table(tidy_data, file="tidy_data.txt")
 
 subjects <- c(1, 11, 14, 15, 16, 17, 19, 21, 22, 23, 25, 26, 27, 28, 29, 3, 30, 5, 6, 7, 8)
---
+++ run_analysis.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+set -e
+
+R -q -f run_analysis.R
